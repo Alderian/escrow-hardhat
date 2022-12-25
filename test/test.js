8959bdbfe2950a3b2cea4815ec5a62ad934bc58f
Alderian <53636+Alderian@users.noreply.github.com>
2022-12-25 17:31:00 -0300

    format. Migrate test to fixture and add test Approved emited

--- test/test.js
+++ test/test.js
@@ -1,25 +1,31 @@
 const { ethers } = require('hardhat');
 const { expect } = require('chai');
+const { loadFixture } = require('@nomicfoundation/hardhat-network-helpers');
+
+const deposit = ethers.utils.parseEther('1');
 
 describe('Escrow', function () {
-  let contract;
-  let depositor;
-  let beneficiary;
-  let arbiter;
-  const deposit = ethers.utils.parseEther('1');
-  beforeEach(async () => {
-    depositor = ethers.provider.getSigner(0);
-    beneficiary = ethers.provider.getSigner(1);
-    arbiter = ethers.provider.getSigner(2);
+  let contract, depositor, beneficiary, arbiter, addr1, addr2, addr3;
+
+  // We define a fixture to reuse the same setup in every test.
+  // We use loadFixture to run this setup once, snapshot that state,
+  // and reset Hardhat Network to that snapshopt in every test.
+  async function prepareEsrow() {
+    [depositor, beneficiary, arbiter, addr1, addr2, addr3] = await ethers.getSigners();
+
     const Escrow = await ethers.getContractFactory('Escrow');
-    contract = await Escrow.deploy(
-      arbiter.getAddress(),
-      beneficiary.getAddress(),
-      {
-        value: deposit,
-      }
-    );
+    contract = await Escrow.deploy(arbiter.getAddress(), beneficiary.getAddress(), {
+      value: deposit,
+    });
     await contract.deployed();
+
+    return { contract, depositor, beneficiary, arbiter, addr1, addr2, addr3 };
+  }
+
+  before('Any test NFT Marketplace', async () => {
+    ({ contract, depositor, beneficiary, arbiter, addr1, addr2, addr3 } = await loadFixture(
+      prepareEsrow
+    ));
   });
 
   it('should be funded initially', async function () {
@@ -36,8 +42,11 @@ describe('Escrow', function () {
   describe('after approval from the arbiter', () => {
     it('should transfer balance to beneficiary', async () => {
       const before = await ethers.provider.getBalance(beneficiary.getAddress());
+
       const approveTxn = await contract.connect(arbiter).approve();
       await approveTxn.wait();
+      await expect(approveTxn).to.emit(contract, 'Approved').withArgs(deposit);
+
       const after = await ethers.provider.getBalance(beneficiary.getAddress());
       expect(after.sub(before)).to.eq(deposit);
     });
